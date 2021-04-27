e168409cdf40d7c8e541e2c57afd160a5b9b75a8
Jeffrey Guenther <guenther.jeffrey@gmail.com>
2021-04-27 14:22:34 -0700

    Add Config service object

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,8 +1,28 @@
-// Used to access the config.yml file
-// Provides methods to detect if the config exists
-
+import { env } from 'string-env-interpolation';
+import { existsSync, readFileSync } from 'fs';
+import YAML from 'yaml';
 export default class Config {
-  constructor(){
+  environments(): any {
+    return this.parseConfigFile();
+  }
 
+  filePresent(): boolean{
+    return existsSync(this.configPath());
+  }
+
+  private parseConfigFile(): object{
+    return YAML.parse(this.interpolateEnvVariables());
+  }
+
+  private interpolateEnvVariables(): string {
+    return env(this.readConfigFile());
+  }
+
+  private readConfigFile(): string {
+    return readFileSync(this.configPath()).toString();
+  }
+
+  private configPath(): string {
+    return process.cwd() + '/config.yml';
   }
 }

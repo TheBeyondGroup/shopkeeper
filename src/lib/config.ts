428c9072aed9038dcfa70451fc5fdee33a12e0f0
Jeffrey Guenther <guenther.jeffrey@gmail.com>
2021-04-26 17:31:57 -0700

    Stubs service objects
    Here's what I'm thinking some of the services objects should do.

---
+++ src/lib/config.ts
@@ -0,0 +1,8 @@
+// Used to access the config.yml file
+// Provides methods to detect if the config exists
+
+export default class Config {
+  constructor(){
+
+  }
+}
---
+++ src/shopkeeper.ts
@@ -0,0 +1,7 @@
+// This is the main entry point to all the shopkeeper functionality
+// This is also the class to import when using shopkeeper elsewhere.
+
+export default class Shopkeeper {
+  constructor(){
+  }
+}

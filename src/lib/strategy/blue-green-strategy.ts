282c046d9c85b74ecfb5e63176bbd16aeba1aecb
Jeffrey Guenther <guenther.jeffrey@gmail.com>
2021-05-01 23:15:01 -0700

    Implements blue/green deploy command object
    In this commit, I add an a first-pass implementation of this feature.
    It completes a blue/green deploy of theme in the current working
    directory.

---
+++ src/lib/strategy/blue-green-strategy.ts
@@ -0,0 +1,76 @@
+import ShopifyClient from "../shopify-client";
+import themekit from '@shopify/themekit';
+
+export default class BlueGreenStrategy implements Strategy {
+  PRODUCTION_GREEN = 'production-green';
+  PRODUCTION_BLUE = 'production-blue';
+  STAGING = 'staging';
+
+  async deploy(staging: boolean = false){
+    const theme = await this.publishedTheme();
+    const themeId = theme.id.toString();
+    
+    await this.downloadPublishedThemeSettings();
+    
+    // determine destination blue or green
+    const onDeckTheme = this.ondeckTheme(themeId);
+    
+    // deploy to destination
+    console.log(`Deploying to ${onDeckTheme}`)
+    await this.deployEnvironment(onDeckTheme);
+
+    if(staging){
+      await this.deployEnvironment(this.STAGING);
+    }
+  }
+
+  private async deployEnvironment(environment: string){
+    const flags = {
+      env: environment,
+    };
+    await themekit.command('deploy', flags);
+  }
+
+  private async downloadPublishedThemeSettings(){
+    console.log(`Downloading settings from live theme`)
+    const flags = {
+      files: ['config/settings_data.json'],
+      live: true,
+      store: this.storeUrl(),
+      password: this.apiPassword()
+    };
+    await themekit.command('download', flags);
+  }
+
+  private async publishedTheme(): Promise<any> {
+    return await this.shopifyClient().getPublishedTheme();
+  }
+
+  private shopifyClient(): ShopifyClient {
+    const client = new ShopifyClient(
+      this.storeUrl(),
+      this.apiPassword()
+    );
+    return client;
+  }
+
+  private storeUrl(): string {
+    return `https://${process.env.PROD_STORE_URL}` || "";
+  }
+
+  private apiPassword(): string {
+    return process.env.PROD_PASSWORD || "";
+  }
+
+  private isBlueTheme(themeId: string): boolean {
+    return process.env.PROD_BLUE_THEME_ID === themeId;
+  }
+
+  private ondeckTheme(publishedThemeId: string): string {
+    if(this.isBlueTheme(publishedThemeId)){
+      return this.PRODUCTION_GREEN;
+    }else{
+      return this.PRODUCTION_BLUE;
+    }
+  }
+}
---
+++ src/lib/strategy/strategy.ts
@@ -0,0 +1,3 @@
+interface Strategy {
+  deploy(): void
+}

f48b9a01f831dfed8819a31b35a3a57f8bc41043
Jeffrey Guenther <guenther.jeffrey@gmail.com>
2021-05-04 14:16:28 -0700

    Add updateTheme method
    We'll use this method to update the theme's name.

--- src/lib/shopify-client.ts
+++ src/lib/shopify-client.ts
@@ -8,6 +8,12 @@ type CreateThemePayload = {
   }
 }
 
+type UpdateThemeNamePayload = {
+  theme: {
+    name: string
+  }
+}
+
 export default class ShopifyClient {
   API_VERSION: string = '2021-04';
   PUBLISHED: string = 'main';
@@ -57,6 +63,18 @@ export default class ShopifyClient {
     }
   }
 
+  async updateTheme(themeId: string, name: string): Promise<any>{
+    try{
+      const { data }  = await this.update(this.themePath(themeId), { 
+        theme: { name: name }
+      });
+
+      return data;
+    }catch(error){
+      console.log(error)
+    }
+  }
+
   private themesPath(): string {
     return `/admin/api/${this.API_VERSION}/themes.json`
   }
@@ -69,6 +87,9 @@ export default class ShopifyClient {
   private async create(path: string, payload: CreateThemePayload) {
     return await this.post(path, payload);
   }
+
+  private async update(path: string, payload: UpdateThemeNamePayload) {
+    return await this.put(path, payload);
   }
 
   private async get(path: string) {

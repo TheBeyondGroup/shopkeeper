1edb57d5ed295c002e8eac83b28bc66b054ef492
Jeffrey Guenther <guenther.jeffrey@gmail.com>
2022-03-03 17:07:40 -0800

    Add flag to preserve settings to settings restore command
    When settings are restored, we want the ability to control whether all the
    settings are replaced or copied. When the settings are copied, we
    preserve the existing files and only overwrite the ones for which we
    have a corresponding environment version.

--- src/cli/shopkeeper-theme-settings-restore.ts
+++ src/cli/shopkeeper-theme-settings-restore.ts
@@ -5,24 +5,40 @@ import fs  from 'fs-extra';
 import ShopkeeperConfig from '../lib/shopkeeper-config';
 
 program
-  .description("restore store theme settings")
-  .argument("[store]", "the name of the store environment")
+  .description("restore environment's settings")
+  .argument("<environment>", "the environment")
+  .option(
+    '-c --copy', 
+    'preserve contents of theme when restoring settings. By default, settings are replaced.',
+    false
+  )
 
-program.action(async(store) => {
+const options = program.opts();
+
+program.action(async(environment) => {
   const config = new ShopkeeperConfig();
-  let storeToRestore = store
+  let backupEnv = environment
 
-  if(!storeToRestore){
-    storeToRestore = await config.getCurrentStore()
+  if(!backupEnv){
+    backupEnv = await config.getCurrentEnvironment()
   }
 
   try {
-    const fileMoves = await config.backupThemeSettingsRestoreFileMoves(storeToRestore)
+    console.log(`Restoring settings for ${backupEnv}.`)
+    // Remove all JSON files from shopify/templates
+    if(!options.copy) {
+      const themeJSONTemplates = await config.themeJSONTemplates()
+      themeJSONTemplates.forEach(fileName => {
+        fs.rm(fileName)
+        console.log(`Deleted ${fileName}`)
+      })
+    }
+
+    const fileMoves = await config.backupThemeSettingsRestoreFileMoves(backupEnv)
     fileMoves.forEach(async ({source, destination}) => {
       await fs.copy(source, destination)
       console.log(`Copied ${source} to ${destination}`)
     })
-    console.log(`Restored settings for ${storeToRestore}.`)
   } catch (err) {
     console.log(err)
     process.exit(1)

956031013442c7120bc8505dd0f91588fb88d636
Jeffrey Guenther <guenther.jeffrey@gmail.com>
2022-03-03 16:59:02 -0800

    Clarify setting root command help messages

--- src/cli/shopkeeper-theme-settings.ts
+++ src/cli/shopkeeper-theme-settings.ts
@@ -6,8 +6,8 @@ const program = new Command();
 
 program
   .command('download', 'download theme settings')
-  .command('save', 'save theme settings to shopkeeper cache')
-  .command('restore', 'restore theme settings from shopkeeper cache')
+  .command('save', 'save theme settings to environment')
+  .command('restore', 'restore theme settings from environment')
   // .command('upload', 'upload theme settings')
 
 program.action(() => {

eaa139ea700ce14ebca82bad910e7269b76d0faa
Jeffrey Guenther <guenther.jeffrey@gmail.com>
2023-02-01 14:42:24 -0800

    Stub Shopkeeper commands
    Stub an outline of the commands we'll add to the tool. I haven't
    regenerated the docs yet because I still need to implement args and
    flags. That'll come during a documentation pass.

---
+++ src/commands/environment/init.ts
@@ -0,0 +1,13 @@
+import { Command } from "@oclif/core";
+
+export default class Init extends Command {
+  static description = "";
+
+  static examples = [];
+
+  static flags = {};
+
+  static args = {};
+
+  async run(): Promise<void> { }
+}
---
+++ src/commands/environment/restore.ts
@@ -0,0 +1,13 @@
+import { Command } from "@oclif/core";
+
+export default class Restore extends Command {
+  static description = "";
+
+  static examples = [];
+
+  static flags = {};
+
+  static args = {};
+
+  async run(): Promise<void> { }
+}

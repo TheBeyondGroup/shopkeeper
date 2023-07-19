8a0ef49246af9b8e5b75a3c55f8e7ecdd1c0f9d3
Jeffrey Guenther <guenther.jeffrey@gmail.com>
2023-07-19 16:38:45 -0700

    Stub the command surface area.

---
+++ src/commands/bucket/create.ts
@@ -0,0 +1,11 @@
+import { Command } from "@oclif/core";
+
+export default class Create extends Command {
+  static description = "";
+
+  static examples = [];
+
+  static flags = {};
+
+  async run(): Promise<void> {}
+}

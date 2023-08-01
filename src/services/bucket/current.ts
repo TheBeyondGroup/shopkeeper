880897d3666a24a6af9bdf5738f2e04edfefef49
Jeffrey Guenther <guenther.jeffrey@gmail.com>
2023-08-01 16:28:55 -0700

    Clean up bucket create and bucket current
    We have utility functions now that we can make use, so let's expose less
    of the internals.

--- src/services/bucket/current.ts
+++ src/services/bucket/current.ts
@@ -1,28 +1,20 @@
-import { findPathUp, fileExistsSync, readFile } from "@shopify/cli-kit/node/fs";
+import { fileExistsSync, readFile, renameFile } from "@shopify/cli-kit/node/fs";
+import { renderInfo } from "@shopify/cli-kit/node/ui";
+import { getShopkeeperPath } from "../../utilities/bucket.js";
 import {
   currentBucketFile,
-  legacyCurrentBucketFile,
-  shopkeeperDirectory,
-} from "../../utilities/index.js";
-import { renderInfo } from "@shopify/cli-kit/node/ui";
-import { renameSync } from "fs";
-import { AbortError } from "@shopify/cli-kit/node/error";
+  legacyCurrentBucketFile
+} from "../../utilities/constants.js";
 
 export async function current() {
-  const bucketRoot = await findPathUp(`./${shopkeeperDirectory}`, {
-    type: "directory",
-  });
-
-  if (bucketRoot === undefined) {
-    throw new AbortError("Cannot find .shopkeeper directory.");
-  }
+  const shopkeeperRoot = await getShopkeeperPath()
 
   let currentBucket: string;
-  const legacyBucketPath = `${bucketRoot}/${legacyCurrentBucketFile}`;
-  const currentBucketPath = `${bucketRoot}/${currentBucketFile}`;
+  const legacyBucketPath = `${shopkeeperRoot}/${legacyCurrentBucketFile}`;
+  const currentBucketPath = `${shopkeeperRoot}/${currentBucketFile}`;
   if (fileExistsSync(legacyBucketPath)) {
     currentBucket = await readFile(legacyBucketPath);
-    renameSync(legacyBucketPath, currentBucketPath);
+    await renameFile(legacyBucketPath, currentBucketPath);
   } else {
     currentBucket = await readFile(currentBucketPath);
   }

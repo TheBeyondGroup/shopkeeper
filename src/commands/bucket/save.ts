c9eca16ab5ec8fc3b7f13c5066b7a024b9b33ebb
Jeffrey Guenther <guenther.jeffrey@gmail.com>
2023-08-01 16:25:01 -0700

    Implement the bucket save command
    This command copies .json settings files from the theme into the
    specified shopkeeper bucket. If a bucket is not passed for some reason,
    we prompt the user to select one of the existing buckets.
    
    A nice clean output message is shown after the task completes

--- src/commands/bucket/save.ts
+++ src/commands/bucket/save.ts
@@ -1,11 +1,47 @@
-import { Command } from "@oclif/core";
+import { Args, Flags } from "@oclif/core";
+import BaseCommand from "@shopify/cli-kit/node/base-command";
+import { globalFlags } from '@shopify/cli-kit/node/cli';
+import { renderSuccess } from "@shopify/cli-kit/node/ui";
+import { themeFlags } from '@shopify/theme/dist/cli/flags.js';
+import { save } from "../../services/bucket/save.js";
+import { getBucketByPrompt } from "../../utilities/bucket.js";
 
-export default class Save extends Command {
-  static description = "";
+export default class Save extends BaseCommand {
+  static description = "Saves the current theme settings to the specified bucket";
 
-  static examples = [];
+  static args = {
+    bucket: Args.string({
+      name: "bucket",
+      description: "The bucket where you want to save your settings.",
+    })
+  };
 
-  static flags = {};
+  static flags = {
+    ...globalFlags,
+    path: themeFlags.path,
+    environment: themeFlags.environment,
+    nodelete: Flags.boolean({
+      char: "n",
+      default: false,
+      description: "Runs the save command without deleting the bucket's contents.",
+      env: "SHOPIFY_FLAG_NODELETE",
+    }),
+  };
 
-  async run(): Promise<void> { }
+  async run(): Promise<void> {
+    const { args, flags } = await this.parse(Save)
+    const bucket = args.bucket || await getBucketByPrompt()
+    const fileMoves = await save(bucket, flags.path, flags.nodelete)
+
+    renderSuccess({
+      headline: `Saved settings to ${bucket}:`,
+      body: [
+        {
+          list: {
+            items: fileMoves.map(move => move.file)
+          },
+        },
+      ],
+    })
+  }
 }

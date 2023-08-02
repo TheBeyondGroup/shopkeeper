c45a07ffbac8425ffd1ef75d3ff3573a747f34e7
Jeffrey Guenther <guenther.jeffrey@gmail.com>
2023-08-02 14:47:25 -0700

    Refactor bucket init to use constant for directory

--- src/services/bucket/init.ts
+++ src/services/bucket/init.ts
@@ -1,6 +1,6 @@
 import { mkdir } from "@shopify/cli-kit/node/fs"
+import { shopkeeperDirectory } from "../../utilities/constants.js"
 
 export async function init() {
-  mkdir("./.shopkeeper")
+  mkdir(`./${shopkeeperDirectory}`)
 }
-

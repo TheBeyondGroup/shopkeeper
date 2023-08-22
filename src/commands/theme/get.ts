6ce1f11f4028f27f48d6015451ee33ef498cf1de
Jeffrey Guenther <guenther.jeffrey@gmail.com>
2023-08-22 13:24:58 -0700

    Implement theme get command
    This command is helpful when building CI systems when you need to get
    the id for a theme by name.

--- src/commands/theme/get.ts
+++ src/commands/theme/get.ts
@@ -1,11 +1,47 @@
-import { Command } from "@oclif/core";
+import { Flags } from '@oclif/core';
+import BaseCommand from "@shopify/cli-kit/node/base-command";
+import { globalFlags } from "@shopify/cli-kit/node/cli";
+import { ensureAuthenticatedThemes } from "@shopify/cli-kit/node/session";
+import { themeFlags } from "@shopify/theme/dist/cli/flags.js";
+import { ensureThemeStore } from "@shopify/theme/dist/cli/utilities/theme-store.js";
+import { get } from "../../services/theme/get.js";
 
-export default class Get extends Command {
-  static description = "";
+export default class Get extends BaseCommand {
+  static description = "Get details of theme";
 
-  static examples = [];
+  static flags = {
+    ...globalFlags,
+    store: themeFlags.store,
+    password: themeFlags.password,
+    theme: Flags.string({
+      char: 't',
+      required: true,
+      description: 'Theme ID or name of the remote theme.',
+      env: 'SHOPIFY_FLAG_THEME_ID',
+    }),
+    json: Flags.boolean({
+      char: 'j',
+      description: 'Output JSON instead of a UI.',
+      env: 'SHOPIFY_FLAG_JSON',
+    }),
+  };
 
-  static flags = {};
+  async run(): Promise<void> {
+    const { flags } = await this.parse(Get)
+    const store = ensureThemeStore(flags)
+    const adminSession = await ensureAuthenticatedThemes(store, flags.password)
 
-  async run(): Promise<void> { }
+    const themes = await get(adminSession, flags.theme!)
+
+    if (flags.json) {
+      themes.map(theme => this.logJson({
+        id: theme.id,
+        name: theme.name,
+        role: theme.role,
+        development: theme.hasDevelopmentRole
+      }))
+    } else {
+      themes.map(theme => this.log(`${theme.id}`))
+    }
+  }
 }
---
+++ src/services/theme/get.ts
@@ -0,0 +1,15 @@
+import { AdminSession } from "@shopify/cli-kit/node/session";
+import { Theme } from "@shopify/cli-kit/node/themes/models/theme.js";
+import { fetchStoreThemes } from "@shopify/theme/dist/cli/utilities/theme-selector/fetch.js";
+import { Filter, filterThemes } from "@shopify/theme/dist/cli/utilities/theme-selector/filter.js";
+
+export async function get(adminSession: AdminSession, theme: string): Promise<Theme[]> {
+  let storeThemes = await fetchStoreThemes(adminSession)
+  const filter = new Filter({ theme: theme })
+
+  if (filter.any()) {
+    storeThemes = filterThemes(adminSession.storeFqdn, storeThemes, filter)
+  }
+
+  return storeThemes
+}

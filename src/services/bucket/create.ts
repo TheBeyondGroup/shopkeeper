4aed920fd7ede2e8705dd91ee7ae955523a16a9c
Jeffrey Guenther <guenther.jeffrey@gmail.com>
2023-08-25 14:41:15 -0700

    Add default content to .env and .env.sample

--- src/services/bucket/create.ts
+++ src/services/bucket/create.ts
@@ -1,21 +1,18 @@
 import { pluralize } from "@shopify/cli-kit/common/string"
-import { mkdir, touchFile } from "@shopify/cli-kit/node/fs"
+import { mkdir, writeFile } from "@shopify/cli-kit/node/fs"
 import { renderSuccess } from "@shopify/cli-kit/node/ui"
 import { getBucketPath } from "../../utilities/bucket.js"
-import { SHOPKEEPER_DIRECTORY } from "../../utilities/constants.js"
 
 export async function create(buckets: string[]) {
-
-  buckets.forEach(async bucket => {
+  await Promise.all(buckets.map(async bucket => {
     const bucketPath = await getBucketPath(bucket)
     await mkdir(bucketPath)
-    await touchFile(`${bucketPath}/.env`)
-    await touchFile(`${bucketPath}/.env.sample`)
+    await writeFile(`${bucketPath}/.env`, envContent())
+    await writeFile(`${bucketPath}/.env.sample`, envContent())
     await mkdir(`${bucketPath}/config`)
     await mkdir(`${bucketPath}/templates`)
     await mkdir(`${bucketPath}/sections`)
-  })
-
+  }))
   renderSuccess({
     body: pluralize(
       buckets,
@@ -23,4 +20,16 @@ export async function create(buckets: string[]) {
       (bucket) => [`${bucket} was created.`]
     )
   })
+
+}
+
+function envContent() {
+  return [
+    "SHOPIFY_CLI_THEME_TOKEN=<Theme Access token>",
+    "SHOPIFY_FLAG_STORE=<Shopify store url>",
+    "SHOPIFY_FLAG_PATH=<path to folder containing theme>",
+    "# Set these flags if you're using the blue/green strategy",
+    "# SKR_FLAG_GREEN_THEME_ID=<theme ID>",
+    "# SKR_FLAG_BLUE_THEME_ID=<theme ID>"
+  ].join("\n")
 }

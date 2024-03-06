b710c4cbd0a249e91c3e6d3109367bd8e9f2405a
Jeffrey Guenther <guenther.jeffrey@gmail.com>
2024-03-05 17:23:07 -0800

    feat: implement current bucket tests

--- src/services/bucket/current.ts
+++ src/services/bucket/current.ts
@@ -1,24 +1,12 @@
-import {fileExistsSync, readFile, renameFile} from '@shopify/cli-kit/node/fs'
-import {renderInfo} from '@shopify/cli-kit/node/ui'
-import {getShopkeeperPath} from '../../utilities/bucket.js'
-import {CURRENT_BUCKET_FILE, LEGACY_CURRENT_BUCKET_FILE} from '../../utilities/constants.js'
-import {joinPath} from '@shopify/cli-kit/node/path'
+import { renderInfo } from '@shopify/cli-kit/node/ui'
+import { getCurrentBucket, getShopkeeperPath } from '../../utilities/bucket.js'
 
-export async function current() {
-  const shopkeeperRoot = await getShopkeeperPath()
-
-  let currentBucket: string
-  const legacyBucketPath = joinPath(shopkeeperRoot, LEGACY_CURRENT_BUCKET_FILE)
-  const currentBucketPath = joinPath(shopkeeperRoot, CURRENT_BUCKET_FILE)
-  if (fileExistsSync(legacyBucketPath)) {
-    currentBucket = await readFile(legacyBucketPath)
-    await renameFile(legacyBucketPath, currentBucketPath)
-  } else {
-    currentBucket = await readFile(currentBucketPath)
-  }
+export async function current(rootPath: string | null = null) {
+  const shopkeeperRoot = rootPath || await getShopkeeperPath()
+  const currentBucket = await getCurrentBucket(shopkeeperRoot)
 
   renderInfo({
     headline: 'Current bucket',
-    body: `${currentBucket.trim()} is selected`,
+    body: `${currentBucket} is selected`,
   })
 }

08c7c1c4bc905539d5c9a7711a8d1c58af5b5567
Jeffrey Guenther <guenther.jeffrey@gmail.com>
2024-03-06 14:59:44 -0800

    feat: implement init service tests

--- src/services/bucket/init.ts
+++ src/services/bucket/init.ts
@@ -1,23 +1,25 @@
-import {appendFile, fileExists, mkdir, writeFile} from '@shopify/cli-kit/node/fs'
-import {joinPath} from '@shopify/cli-kit/node/path'
-import {renderSuccess} from '@shopify/cli-kit/node/ui'
-import {createBuckets} from '../../utilities/bucket.js'
-import {SHOPKEEPER_DIRECTORY} from '../../utilities/constants.js'
+import { appendFile, fileExists, mkdir, writeFile } from '@shopify/cli-kit/node/fs'
+import { joinPath } from '@shopify/cli-kit/node/path'
+import { renderSuccess } from '@shopify/cli-kit/node/ui'
+import { createBuckets, getShopkeeperPath } from '../../utilities/bucket.js'
+import { SHOPKEEPER_DIRECTORY } from '../../utilities/constants.js'
 
-export async function init(buckets: string[]) {
-  await mkdir(joinPath('.', SHOPKEEPER_DIRECTORY))
+export async function init(buckets: string[], cwd?: string) {
+  const projectRoot = cwd || '.'
+  await mkdir(joinPath(projectRoot, SHOPKEEPER_DIRECTORY))
+  const shopkeeperRoot = await getShopkeeperPath(projectRoot)
 
   let action = ''
-  const gitIgnorePath = joinPath('.', '.gitignore')
+  const gitIgnorePath = joinPath(projectRoot, '.gitignore')
   if (await fileExists(gitIgnorePath)) {
-    await appendFile(gitIgnorePath, DEFAULT_GITIGNORE_FILE)
+    await appendFile(gitIgnorePath, '\n\n' + DEFAULT_GITIGNORE_FILE)
     action = 'updated'
   } else {
     await writeFile(gitIgnorePath, DEFAULT_GITIGNORE_FILE)
     action = 'created'
   }
 
-  await createBuckets(buckets)
+  await createBuckets(shopkeeperRoot, buckets)
 
   renderSuccess({
     headline: 'Shopkeeper initialized',
@@ -47,7 +49,7 @@ function renderBucketListSection(buckets: string[]) {
   }
 }
 
-const DEFAULT_GITIGNORE_FILE = `# Shopkeeper #
+export const DEFAULT_GITIGNORE_FILE = `# Shopkeeper #
 ##########
 .shopkeeper/**/**/.env
 .shopkeeper/.current-bucket

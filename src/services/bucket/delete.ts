d44c8e23d917ae8e24e1c23506a650d18e4b061e
Jeffrey Guenther <guenther.jeffrey@gmail.com>
2024-03-06 14:59:05 -0800

    feat: implement delete service tests

--- src/services/bucket/delete.ts
+++ src/services/bucket/delete.ts
@@ -1,25 +1,27 @@
-import {pluralize} from '@shopify/cli-kit/common/string'
-import {rmdir} from '@shopify/cli-kit/node/fs'
-import {renderConfirmationPrompt, renderInfo, renderSuccess} from '@shopify/cli-kit/node/ui'
-import {getBucketPath} from '../../utilities/bucket.js'
+import { pluralize } from '@shopify/cli-kit/common/string'
+import { rmdir } from '@shopify/cli-kit/node/fs'
+import { renderConfirmationPrompt, renderInfo, renderSuccess } from '@shopify/cli-kit/node/ui'
+import { getBucketPath, getShopkeeperPath } from '../../utilities/bucket.js'
 
-export async function deleteBucket(buckets: string[], force: Boolean) {
+export async function deleteBucket(buckets: string[], force: Boolean, rootPath?: string) {
+  const shopkeeperRoot = rootPath || await getShopkeeperPath()
+
   let didDelete = false
   if (force) {
     await Promise.all(
       buckets.map(async (bucket) => {
-        const bucketPath = await getBucketPath(bucket)
-        return await rmdir(bucketPath, {force: true})
+        const bucketPath = await getBucketPath(shopkeeperRoot, bucket)
+        return await rmdir(bucketPath, { force: true })
       }),
     )
     didDelete = true
   } else {
-    const shouldDelete = await renderConfirmationPrompt({message: `Do you want to delete ${buckets.join(' ')}`})
+    const shouldDelete = await renderConfirmationPrompt({ message: `Do you want to delete ${buckets.join(' ')}` })
     if (shouldDelete) {
       await Promise.all(
         buckets.map(async (bucket) => {
-          const bucketPath = await getBucketPath(bucket)
-          return await rmdir(bucketPath, {force: true})
+          const bucketPath = await getBucketPath(shopkeeperRoot, bucket)
+          return await rmdir(bucketPath, { force: true })
         }),
       )
     }
@@ -29,11 +31,11 @@ export async function deleteBucket(buckets: string[], force: Boolean) {
     renderSuccess({
       body: pluralize(
         buckets,
-        (buckets) => ['The following buckets were deleted:', {list: {items: buckets}}],
+        (buckets) => ['The following buckets were deleted:', { list: { items: buckets } }],
         (bucket) => [`${bucket} was deleted.`],
       ),
     })
   } else {
-    renderInfo({body: 'Delete skipped'})
+    renderInfo({ body: 'Delete skipped' })
   }
 }

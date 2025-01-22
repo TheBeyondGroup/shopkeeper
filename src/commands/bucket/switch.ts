176f76a36023239b7b55af8d6b3d9c82bea20051
Jeffrey Guenther <jeff@thebeyondgroup.la>
2025-01-22 15:43:05 -0800

    chore: update bucket switch
    We need to update the imports after removing the
    @shopify/theme.

--- src/commands/bucket/switch.ts
+++ src/commands/bucket/switch.ts
@@ -1,35 +1,35 @@
-import { Args, Flags } from "@oclif/core";
-import BaseCommand from "@shopify/cli-kit/node/base-command";
-import { globalFlags } from '@shopify/cli-kit/node/cli';
-import { renderSuccess } from "@shopify/cli-kit/node/ui";
-import { themeFlags } from '@shopify/theme/dist/cli/flags.js';
-import { switchBucket } from "../../services/bucket/switch.js";
-import { getBucketByPrompt, getShopkeeperPath } from "../../utilities/bucket.js";
+import {Flags} from '@oclif/core'
+import BaseCommand from '@shopify/cli-kit/node/base-command'
+import {globalFlags} from '@shopify/cli-kit/node/cli'
+import {renderSuccess} from '@shopify/cli-kit/node/ui'
+import {switchBucket} from '../../services/bucket/switch.js'
+import {getBucketByPrompt, getShopkeeperPath} from '../../utilities/bucket.js'
+import {themeFlags} from '../../utilities/shopify/flags.js'
 
 export default class Switch extends BaseCommand {
-  static description = "Switches the current bucket by copying settings and .env";
+  static description = 'Switches the current bucket by copying settings and .env'
 
   static flags = {
     ...globalFlags,
     path: themeFlags.path,
     environment: themeFlags.environment,
     bucket: Flags.string({
-      name: "bucket",
-      description: "The bucket to switch to",
+      name: 'bucket',
+      description: 'The bucket to switch to',
     }),
     nodelete: Flags.boolean({
-      char: "n",
+      char: 'n',
       default: false,
       description: "Runs the restore command without removing the theme's JSON settings.",
-      env: "SHOPIFY_FLAG_NODELETE",
-    })
+      env: 'SHOPIFY_FLAG_NODELETE',
+    }),
   }
 
   async run(): Promise<void> {
     const shopKeeperRoot = await getShopkeeperPath()
 
-    const { flags } = await this.parse(Switch);
-    const bucket = flags.bucket || await getBucketByPrompt(shopKeeperRoot)
+    const {flags} = await this.parse(Switch)
+    const bucket = flags.bucket || (await getBucketByPrompt(shopKeeperRoot))
     const fileMoves = await switchBucket(bucket, flags.path, flags.nodelete)
 
     renderSuccess({
@@ -37,7 +37,7 @@ export default class Switch extends BaseCommand {
       body: [
         {
           list: {
-            items: fileMoves.map(move => move.file)
+            items: fileMoves.map((move) => move.file),
           },
         },
       ],

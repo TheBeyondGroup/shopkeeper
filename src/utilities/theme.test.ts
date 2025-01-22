ba9936f3413a4fe9844750fea104eb883fa5c59c
Jeffrey Guenther <jeff@thebeyondgroup.la>
2025-01-22 15:39:18 -0800

    feat: update theme utility

--- src/utilities/theme.test.ts
+++ src/utilities/theme.test.ts
@@ -1,88 +1,134 @@
-import { describe, expect, test, vi } from 'vitest'
-import { execCLI2 } from '@shopify/cli-kit/node/ruby'
-import { getThemesByIdentifier, pullLiveThemeSettings, push, pushToLive } from './theme.js'
-import { fetchStoreThemes } from "@shopify/theme/dist/cli/utilities/theme-selector/fetch.js"
-import { Theme } from '@shopify/cli-kit/node/themes/types'
+import {describe, expect, test, vi} from 'vitest'
+import {deployTheme, deployToLive, getThemesByIdentifier, pullLiveThemeSettings, pullThemeSettings} from './theme.js'
+import {fetchStoreThemes, pull, push} from '@shopify/cli'
+import {Theme} from '@shopify/cli-kit/node/themes/types'
+import {PullFlags} from './shopify/services/pull.js'
+import {PushFlags} from './shopify/services/push.js'
 
-vi.mock('@shopify/cli-kit/node/ruby')
-vi.mock("@shopify/theme/dist/cli/utilities/theme-selector/fetch.js")
+vi.mock('@shopify/cli')
 
 describe('theme utilities', () => {
-  const adminSession = { token: 'ABC', storeFqdn: 'example.myshopify.com' }
+  const adminSession = {token: 'ABC', storeFqdn: 'example.myshopify.com'}
   const path = '/my-theme'
 
-  function expectCLI2ToHaveBeenCalledWith(command: string) {
-    expect(execCLI2).toHaveBeenCalledWith(command.split(' '), {
-      adminToken: adminSession.token,
-      store: 'example.myshopify.com',
-    })
-  }
   describe('pullThemeSettings', () => {
-    test('pulls settings from live theme', async () => {
+    test('pulls settings from theme', async () => {
       // Given
-      const themeFlags = ['--nodelete']
       const settingsFlags = [
-        '--only config/settings_data.json',
-        '--only sections/*.json',
-        '--only templates/*.json',
-        '--only templates/customers/*.json',
-        '--only templates/metaobject/*.json'
-      ].join(" ")
-
+        'config/settings_data.json',
+        'sections/*.json',
+        'templates/*.json',
+        'templates/customers/*.json',
+        'templates/metaobject/*.json',
+      ]
+      const flags: PullFlags = {
+        only: settingsFlags,
+        nodelete: true,
+      }
 
       // When
-      await pullLiveThemeSettings(adminSession, path, themeFlags)
+      await pullThemeSettings(flags)
 
       // Then
-      expectCLI2ToHaveBeenCalledWith(`theme pull ${path} --nodelete --live ${settingsFlags}`)
+      const expectedFlags = {
+        only: settingsFlags,
+        nodelete: true,
+      }
+      expect(pull).toHaveBeenCalledWith(expectedFlags)
     })
   })
-  describe('push', () => {
+
+  describe('pullLiveThemeSettings', () => {
+    test('pulls settings from live theme', async () => {
+      // Given
+      const settingsFlags = [
+        'config/settings_data.json',
+        'sections/*.json',
+        'templates/*.json',
+        'templates/customers/*.json',
+        'templates/metaobject/*.json',
+      ]
+      const flags: PullFlags = {
+        only: settingsFlags,
+        nodelete: true,
+      }
+
+      // When
+      await pullLiveThemeSettings(flags)
+
+      // Then
+      const expectedFlags = {
+        only: settingsFlags,
+        nodelete: true,
+        live: true,
+      }
+      expect(pull).toHaveBeenCalledWith(expectedFlags)
+    })
+  })
+
+  describe('deploy', () => {
     describe('when should publish automatically', () => {
       test('pushes theme files to theme', async () => {
         // Given
-        const themeFlags = ['--nodelete']
         const themeId = 1
+        const flags: PushFlags = {
+          nodelete: true,
+          publish: true,
+        }
 
         // When
-        await push(adminSession, path, true, themeFlags, themeId)
+        await deployTheme(themeId, flags)
 
         // Then
-        expectCLI2ToHaveBeenCalledWith(`theme push ${path} --nodelete --theme ${themeId} --publish`)
+        const expectedFlags = {
+          nodelete: true,
+          publish: true,
+          theme: themeId.toString(),
+        }
+        expect(push).toHaveBeenCalledWith(expectedFlags)
       })
     })
 
     describe('when should not publish automatically', () => {
       test('pushes theme files to theme', async () => {
         // Given
-        const themeFlags = ['--nodelete']
         const themeId = 1
+        const flags: PushFlags = {
+          nodelete: true,
+        }
 
         // When
-        await push(adminSession, path, false, themeFlags, themeId)
+        await deployTheme(themeId, flags)
 
         // Then
-        expectCLI2ToHaveBeenCalledWith(`theme push ${path} --nodelete --theme ${themeId}`)
-
+        const expectedFlags = {
+          nodelete: true,
+          theme: themeId.toString(),
+        }
+        expect(push).toHaveBeenCalledWith(expectedFlags)
       })
     })
   })
-  describe('pushToLive', () => {
+  describe('deployToLive', () => {
     test('pushes theme files to live theme', async () => {
       // Given
-      const themeFlags = ['--nodelete']
+      const flags: PushFlags = {}
 
       // When
-      await pushToLive(adminSession, path, themeFlags)
+      await deployToLive(flags)
 
       // Then
-      expectCLI2ToHaveBeenCalledWith(`theme push ${path} --nodelete --live --allow-live`)
+      const expectedFlags: PushFlags = {
+        live: true,
+        allowLive: true,
+      }
+      expect(push).toHaveBeenCalledWith(expectedFlags)
     })
   })
 
   describe('getThemesByIdentifier', () => {
     function theme(id: number, role: string) {
-      return { id, role, name: `theme (${id})` } as Theme
+      return {id, role, name: `theme (${id})`} as Theme
     }
 
     test('returns list of themes that match id or name', async () => {
@@ -91,7 +137,7 @@ describe('theme utilities', () => {
       vi.mocked(fetchStoreThemes).mockResolvedValue(themes)
 
       // When
-      const result = await getThemesByIdentifier(adminSession, '1')
+      const result = await getThemesByIdentifier(adminSession.storeFqdn, adminSession.token, '1')
 
       // Then
       expect(result).toEqual([themes[0]])
